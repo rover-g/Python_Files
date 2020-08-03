12096994aae63e33effd3cb3e7b5a8c55ef7c43c
rover-g <rajmanu.gattu@gmail.com>
2020-08-03 22:08:42 +0530

    Update 02
    Updated script with export path including the exact date and exact output location

--- Idle_artist_app.py
+++ Idle_artist_app.py
@@ -1,5 +1,9 @@
 import pandas as pd 
-
+import numpy as np
+from pathlib import Path
+from pandas.tseries.offsets import *
+from datetime import datetime
+import pandas.io.formats.excel
 
 sch_input = input('Please enter the path : ')
 atnd_input = input('Please enter the path : ')
@@ -25,6 +29,21 @@ def idle_list(): # concats both the sheet and gives the final output as desired
     idle_data = pd.concat([sch_data(),atnd_data()], axis = 0)
     idle_data.sort_values(by = ['Assigned_To','End'], inplace = True ,ascending = [1,0])
     idle_data.drop_duplicates(subset = ["Assigned_To"], inplace = True)
-    return idle_data 
+    idle_data['End'] = pd.to_datetime(idle_data['End'])
+    idle_data['Free_from'] = idle_data['End'] + BDay()
+    idle_data = (idle_data[['Assigned_To', 'Free_from']])
+    idle_data.sort_values(by = ['Free_from'], inplace = True , ascending = True)
+    idle_data = idle_data.reset_index()
+    idle_data.index = np.arange(1,len(idle_data)+1)
+    idle_data = (idle_data[['Assigned_To', 'Free_from']])
+    path = Path(r'C:\Users\rahul\Documents\DNEG_Works\Idle_list\New_Idle_Artist_List.xlsx')
+    datestring = datetime.now().strftime('%Y-%m-%d')
+    exportpath = path.parent/f'{path.stem}-{datestring}{path.suffix}'
+    writer = pd.ExcelWriter(exportpath,engine='xlsxwriter',datetime_format='mmm d yyyy')
+    idle_data.to_excel(writer, sheet_name = 'Idle_list')
+    output = writer.save()
+    print('Sucessfully Exported')
+    return output
 
-#print(idle_list())
+
+idle_list()
