a36523598737f99ec7d9bc13ae795d9dbe137f1e
rover-g <rajmanu.gattu@gmail.com>
2020-08-03 17:15:47 +0530

    Updated idle artist application
    Now this can read both the files and merge to give us a final output

--- Idle_artist_app.py
+++ Idle_artist_app.py
@@ -1,7 +1,30 @@
 import pandas as pd 
 
 
-inputpath = input('Please enter the path : ')
+sch_input = input('Please enter the path : ')
+atnd_input = input('Please enter the path : ')
 
-df = pd.read_csv(inputpath)
+def sch_data():
+    sch = pd.read_csv(sch_input)
+    sch.columns = sch.columns.str.replace(" ", "_")
+    sch = sch[['Assigned_To','End']]
+    sch.sort_values(by = ['Assigned_To','End'], inplace = True ,ascending = [1,0])
+    sch.drop_duplicates(subset = ["Assigned_To"], inplace = True)
+    return sch
 
+def atnd_data():
+    atnd = pd.read_csv(atnd_input)
+    atnd.columns = atnd.columns.str.replace(" ", "_")
+    atnd = atnd[['Assigned_To','End_Date']]
+    atnd = atnd.rename({'End_Date':'End'}, axis = 1)
+    atnd.sort_values(by = ['Assigned_To','End'], inplace = True ,ascending = [1,0])
+    atnd.drop_duplicates(subset = ["Assigned_To"], inplace = True)
+    return atnd
+
+def idle_list():
+    idle_data = pd.concat([sch_data(),atnd_data()], axis = 0)
+    idle_data.sort_values(by = ['Assigned_To','End'], inplace = True ,ascending = [1,0])
+    idle_data.drop_duplicates(subset = ["Assigned_To"], inplace = True)
+    return idle_data 
+
+#print(idle_list())
